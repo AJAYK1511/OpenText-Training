4a2018c293f7b12a203d9e2ad7dc83b6b2b22ea2
AJAYK1511 <113664202+AJAYK1511@users.noreply.github.com>
2024-10-15 10:26:58 +0530

    Create AddTest.java

---
+++ JUNIT_Testing/AddTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    public void testAddition() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+}
+
+class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
